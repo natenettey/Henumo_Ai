e0718e275f5a33e1e01d61f66961b8eee68a2cad
natenettey <niibobby4@gmail.com>
2022-10-19 21:42:30 +0000

    added the login middleware

--- server/controllers/auth_controller.js
+++ server/controllers/auth_controller.js
@@ -29,3 +29,43 @@ exports.register= async (req, res)=>{
         })
     }
 }
+
+exports.login = async (req,res)=>{
+     const user = req.body
+
+     //check if user exists in the db
+     const user_exists = await User_Model.findOne({email:user.email})
+     
+     if(user_exists){
+        //compare hashed passwords
+        const passwordCheck = await bcrypt.compare(req.body.password, user_exists.password)
+        if(!passwordCheck){
+            res.json({
+                message:"Invalid username / password"
+            })
+            
+        }else{
+            try {
+                const token = jwt.sign(
+                    {
+                        id:user_exists._id,
+                        username:user_exists.username,
+                        email:user_exists.email
+                    },process.env.JWT_SECRET,
+                    {expiresIn:86400}
+                )
+                res.json({message:"ok",token:"Bearer" + token})
+            } catch (error) {
+                if(error){
+                    console.log(error)
+                    res.json({message:error})
+                }
+            }
+        }
+     }else{
+        res.json({
+            message:"Invalid username / password"
+        })
+     }
+
+}
