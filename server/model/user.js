7037145b4818d7fa3589ccfd669ffc139733b956
natenettey <niibobby4@gmail.com>
2022-10-19 19:02:12 +0000

    added the company key

--- server/model/user.js
+++ server/model/user.js
@@ -3,9 +3,10 @@ const mongoose = require('mongoose')
 const Schema = mongoose.Schema
 const UserSchema  = new Schema(
     {
-        name:{type:String, required:true, maxLength:100},
+        username:{type:String, required:true, maxLength:100},
         email:{type:String, required:true, unique:true, maxLength:100},
-        password:{type:String,required:true, minLength:8,maxLength:100}
+        password:{type:String,required:true, minLength:8,maxLength:100},
+        company:{type:String, minLength:3}
     }
 )
 
