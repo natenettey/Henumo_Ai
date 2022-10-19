5c89a93624010912c3afe99abea6ad7b695083a1
natenettey <niibobby4@gmail.com>
2022-10-19 21:43:44 +0000

    added the routes for the login middleware

--- server/routes/auth_routes.js
+++ server/routes/auth_routes.js
@@ -4,6 +4,7 @@ const router = express.Router();
 const auth_controller = require('../controllers/auth_controller')
 
 router.post('/register',auth_controller.register)
+router.post('/login',auth_controller.login)
 
 
 module.exports = router
