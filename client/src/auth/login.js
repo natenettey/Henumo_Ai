1f0176340644d52aca99b3f1b482e15856351e03
natenettey <niibobby4@gmail.com>
2022-10-23 21:18:10 +0000

    login component

---
+++ client/src/auth/login.js
@@ -0,0 +1,56 @@
+import { useState } from "react"
+
+function Login(){
+
+    const [username, setUsernaame] = useState("")
+    const [userpassword, setPassword] = useState("")
+
+    const SubmitForm=(event)=>{
+        event.preventDefault()
+        const values ={
+            user:username,
+            password:userpassword
+
+        }
+
+        fetch(
+            "http://localhost:8000/account/login",{
+                method:"POST",
+                headers:{
+                    "Content-type":"application/json"
+                },
+                body:JSON.stringify(values)
+            }
+        ).then(response=>{
+             return response.json()
+        }).then(data=>{
+            console.log(data)
+            
+            
+        })
+    }
+
+    const getName = (event)=>{
+        setUsernaame(event.target.value)
+        console.log(username)
+
+    }
+
+    const getPassword = (event)=>{
+        setPassword(event.target.value)
+        console.log(userpassword)
+
+    }
+
+    return(
+        <form onSubmit={SubmitForm}>
+            <input type="text" onChange ={getName} required />
+            <br/>
+            <input type="password" onChange ={getPassword}required />
+            <br/>
+            <button type="submit">Some Clickabel</button>
+        </form>
+    )
+}
+
+export default Login
