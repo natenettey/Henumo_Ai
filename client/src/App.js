b8150845145f9ed056f67d63984799ed6fb40785
natenettey <niibobby4@gmail.com>
2022-10-23 21:17:02 +0000

    added the login route

--- client/src/App.js
+++ client/src/App.js
@@ -1,17 +1,25 @@
 import {useState, useEffect} from "react"
+import {Routes, BrowserRouter as Router, Route} from 'react-router-dom'
+import Login from "./auth/login";
 import './App.css';
 
 function App() {
-  const [data,setData]=useState({})
-  useEffect(()=>{
-    fetch("/home")
-    .then(res=>res.json())
-    .then(data=>setData(data))
-  },[])
+  // const [data,setData]=useState({})
+  // useEffect(()=>{
+  //   fetch("/home")
+  //   .then(res=>res.json())
+  //   .then(data=>setData(data))
+  // },[])
+ 
   return (
     <div className="App">
-      <div>
-      {data.name}</div>
+      <Router>
+         <Routes>
+         
+          <Route   path="/login" exact element={<Login/>}/> 
+          
+         </Routes>
+      </Router>
     </div>
   );
 }
