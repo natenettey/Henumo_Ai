1b572b097009bdfcbb37ffcb21b3bf20796bb42c
Nathan Nettey <niibobby4@gmail.com>
2023-06-01 11:54:38 +0000

    added the route for getting all products

--- server/routes/productRoutes.js
+++ server/routes/productRoutes.js
@@ -4,6 +4,7 @@ const productRoutes = require('../controllers/productController')
 
 router.post('/create-product', productRoutes.addProduct)
 router.post('/fetchproducts',productRoutes.getProducts)
+router.get('/fetch-all-products', productRoutes.getAllProducts)
 
 
 module.exports = router
