70af24985e1232189900491856a09561952b3203
Nathan Nettey <niibobby4@gmail.com>
2023-06-05 15:57:23 +0000

    added the routes for the getSpecificProduct middleware

--- server/routes/productRoutes.js
+++ server/routes/productRoutes.js
@@ -5,6 +5,6 @@ const productRoutes = require('../controllers/productController')
 router.post('/create-product', productRoutes.addProduct)
 router.post('/fetchproducts',productRoutes.getProducts)
 router.get('/fetch-all-products', productRoutes.getAllProducts)
-
+router.get('/fetch-one-product', productRoutes.getSpecificProduct)
 
 module.exports = router
