5e530b766387159982cd4a947e1120a9233e82b0
Nathan Nettey <niibobby4@gmail.com>
2023-06-05 15:56:21 +0000

    added the id to the product schema

--- server/model/productModel.js
+++ server/model/productModel.js
@@ -3,6 +3,10 @@ const Schema = mongoose.Schema
 
 const ProductSchema = new Schema(
     {
+        _id: {
+            type: Schema.Types.ObjectId,
+            default: mongoose.Types.ObjectId
+          },
         name:{type:String, required:true},
         image:{
             type:String,  
