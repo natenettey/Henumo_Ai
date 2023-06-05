c4efcc0d0d0363621d7bec90772051ab7f9089ac
Nathan Nettey <niibobby4@gmail.com>
2023-06-05 15:40:51 +0000

    increased the file suze limit

--- server/app.js
+++ server/app.js
@@ -14,8 +14,8 @@ mongoose.connect(mongoDB,{ useNewUrlParser: true , useUnifiedTopology: true })
 const db = mongoose.connection
 db.on('error', console.error.bind(console, 'MongoDB connection error:'))
 
-app.use(express.json());
-app.use(express.urlencoded({extended:false}))
+app.use(express.json({limit:'10mb'}));
+app.use(express.urlencoded({limit:'10mb',extended:false}))
 app.use(cors())
 app.use('/account',auth_router)
 app.use('/products',product_router)
@@ -25,13 +25,6 @@ app.use(function (req, res, next) {
     next();
  })
 
-
-// app.get('/home',(req,res)=>{
-//     res.json({
-//         name:"name",
-//         age:12
-//     })
-// })
 
 
 app.listen(8000)
